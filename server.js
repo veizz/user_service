80a9cbbbacedb418ccc6728db477b4d3f5d152c9
veizz <veizzsmile@gmail.com>
2024-01-21 23:18:45 +0800

    add tbl_user in sqlite database, add stupid login/logout api

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import Fastify from 'fastify'
 import FastifyVite from '@fastify/vite'
+import BetterSqlite3 from 'better-sqlite3'
+import fastifyBetterSqlite3 from '@punkish/fastify-better-sqlite3'
+import fastifySession from '@fastify/session'
+import fastifyCookie from '@fastify/cookie'
 
 const server = Fastify({
   logger: {
@@ -9,8 +13,21 @@ const server = Fastify({
   }
 })
 
-await server.register(FastifyVite, { 
-  root: import.meta.url, 
+server.register(fastifyCookie)
+server.register(fastifySession, {
+  secret: '8c61b89b7b80621dd09597f0d9b994f3',
+  cookie: {
+    secure: false,
+  }
+})
+const sqlite3Opts = {
+  "class": BetterSqlite3,
+  "pathToDb": "./db/db.sqlite3",
+}
+await server.register(fastifyBetterSqlite3, sqlite3Opts)
+
+await server.register(FastifyVite, {
+  root: import.meta.url,
   renderer: '@fastify/react',
 })
 
@@ -22,6 +39,42 @@ server.decorate('db', {
     'Respond to emails',
     'Write report',
   ]
+})
+
+
+server.get('/api/db/test', (req, reply) => {
+  const rows = server.betterSqlite3.prepare('SELECT id, username FROM tbl_user').all()
+  reply.send({ ok: true, data: rows })
+})
+
+server.get('/api/user/login', (req, reply) => {
+  const { username, password } = req.query
+  const row = server.betterSqlite3
+    .prepare(
+      "SELECT id, username FROM tbl_user WHERE username = ? AND password = ?"
+    )
+    .get(username, password)
+  const ok = row !== undefined
+  if (ok) {
+    console.log('uid', row.id);
+    req.session.set('uid', row.id)
+  }
+  reply.send({ ok, data: row });
+})
+
+server.get('/api/user/logout', (req, reply) => {
+  req.session.set('uid', undefined)
+  reply.send({ ok: true })
+})
+
+server.get('/api/user/status', (req, reply) => {
+  const uid = req.session.get('uid')
+  const ok = uid !== undefined
+  reply.send({ ok, data: { uid } })
+});
+
+server.get('/api/todo/items', (req, reply) => {
+  reply.send({ ok: true, data: server.db.todoList });
 })
 
 server.put('/api/todo/items', (req, reply) => {
